669e95579128d3f91610f0f340a97920e8a67918
Eduardo Hernandez Soto <es50678@gmail.com>
2020-07-05 18:08:18 -0400

    Initialize project using Create React App

---
+++ src/components/shopping-list/shopping-list.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface Props {
+  name: string
+}
+
+export class ShoppingList extends React.Component<Props> {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
